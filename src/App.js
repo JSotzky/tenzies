d4ed952adcef76532aeca8872cf2771c12247bbe
Jason Sotzky <Jasonsotzky@gmail.com>
2022-01-22 18:53:59 -0500

    Added main sections and Dice + Styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Die from './components/die.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <main>
+      <div className='dicebox'>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+            </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/die.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function die(props) {
+  return <div className='dice'>
+            <h2>{props.value}</h2>
+        </div>
+}
+
+export default die;
