537c431e86c989ea5d05a3d9f5fb2e7809d4b133
Jason Sotzky <Jasonsotzky@gmail.com>
2022-01-22 22:51:04 -0500

    Added button, dice objects and randomization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import Die from './components/die.js'
+import React from 'react'
+import {nanoid} from "nanoid"
 
 function App() {
+
+const [diceNumbers, setDiceNumbers] = React.useState(allNewDice())
+
+function allNewDice(){
+  const newArray = []
+  for(let i = 0; i < 10; i++){
+    newArray.push({value: Math.ceil(Math.random() * 6),
+                   isHeld: false,
+                   id: nanoid()
+    })
+  }
+  console.log(newArray)
+  return newArray
+}
+
+function reRoll(){
+  setDiceNumbers(allNewDice())
+}
+
   return (
     <div className="App">
       <main>
-      <div className='dicebox'>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-            </div>
+          <div className='dicebox'>
+              {diceNumbers.map((dice, index) => {
+                return <Die value={dice.value} key={dice.id}/>
+              })}
+          </div>
+          <button className="button" onClick={reRoll}><h2>Roll</h2></button>
       </main>
     </div>
   );
